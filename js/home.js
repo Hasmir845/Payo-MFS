bd893cb3dec20e64533b88f3759d9e230c211d6c
HasmirHassan <hasmirhassan@gmail.com>
2024-09-19 02:00:58 +0600

    money added feature added

---
+++ js/home.js
@@ -0,0 +1,22 @@
+document.getElementById('button-add-money').addEventListener('click',function(event){
+    event.preventDefault();
+    
+    const addMoney = document.getElementById('input-add-money').value;
+    
+    const pinNumber = document.getElementById('pin-number').value;
+    console.log(pinNumber);
+
+    if(pinNumber === '1234'){
+        const balance = document.getElementById('account-balance').innerText;
+
+        const addMoneyNumber = parseFloat(addMoney);
+        const addBalanceNumber = parseFloat(balance);
+        const newBalance = addMoneyNumber + addBalanceNumber;
+        
+        document.getElementById('account-balance').innerText = newBalance;
+        
+    }
+    else{
+        alert("Wrong Pin")
+    }
+})
